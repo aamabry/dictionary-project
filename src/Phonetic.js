55b4ab58328fdb097934469e7b8064a82ac4c53a
flangoria <audrey.mabry90@gmail.com>
2024-08-01 10:26:22 -0400

    Fixed small bugs in links

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="blank" rel="noreferrer">
+            <a href={props.phonetic.audio} target="blank" rel="noreferrer" alt-text="keyword sound">
                 Listen
             </a>
             <span className="text">{props.phonetic.text}</span>
